6913a7a2b3a45d454496b26ab3e0ff9032a51010
Sharear Ahammed <sharearahammed@gmail.com>
2024-05-10 19:41:51 +0600

    update errorpage

--- src/assets/Components/ErrorPage/ErrorPage.jsx
+++ src/assets/Components/ErrorPage/ErrorPage.jsx
@@ -12,7 +12,7 @@ const ErrorPage = () => {
             <p className='text-sm font-medium text-gray-500'>404 error</p>
             <h1 className='mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl'>
               Page not found
-            </h1>
+            </h1> 
             <p className='mt-4 text-gray-500 dark:text-gray-400'>
               Sorry, the page you are looking for doesnt exist.Here are some
               helpful links:
