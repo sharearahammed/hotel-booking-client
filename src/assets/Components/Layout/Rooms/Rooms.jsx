5afc551106f46db21d6f0036d535ac40653b186c
Sharear Ahammed <sharearahammed@gmail.com>
2024-05-14 04:17:55 +0600

    room filtering ui update

--- src/assets/Components/Layout/Rooms/Rooms.jsx
+++ src/assets/Components/Layout/Rooms/Rooms.jsx
@@ -6,19 +6,16 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 const Rooms = () => {
-
   const [rooms, setRooms] = useState([]);
-  const [minPrice, setMinPrice] = useState('');
-  const [maxPrice, setMaxPrice] = useState('');
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
 
   useEffect(() => {
-    axios(`https://hotel-booking-server-psi.vercel.app/rooms?minPrice=${minPrice}&maxPrice=${maxPrice}`)
-    .then(
-      (data) => {
-        setRooms(data.data);
-      }
-    );
-    
+    axios(
+      `https://hotel-booking-server-psi.vercel.app/rooms?minPrice=${minPrice}&maxPrice=${maxPrice}`
+    ).then((data) => {
+      setRooms(data.data);
+    });
   }, [maxPrice, minPrice]);
 
   const handleMinPriceChange = (e) => {
@@ -28,7 +25,6 @@ const Rooms = () => {
   const handleMaxPriceChange = (e) => {
     setMaxPrice(e.target.value);
   };
-  
 
   return (
     <div>
@@ -36,31 +32,45 @@ const Rooms = () => {
         <title>Sunshine City Rooms: </title>
         <link rel="canonical" href="https://www.tacobell.com/" />
       </Helmet>
-      <h1 className="mt-10 text-center text-3xl font-bold text-[18px] lg:text-[30px]">Total Rooms : {rooms.length}</h1>
+      <h1 className="mt-10 text-center text-3xl font-bold text-[18px] lg:text-[30px]">
+        Total Rooms : {rooms.length}
+      </h1>
       <Tabs>
         <div className="container px-6 py-10 mx-auto">
           <div className="text-2xl font-bold flex items-center justify-center">
-          <TabList>
-            <div className="flex text-[12px] lg:text-[25px]">
-            <Tab>All Rooms</Tab>
-            <Tab>Available Rooms</Tab>
-            </div>
-          </TabList>
-          
+            <TabList>
+              <div className="flex text-[12px] lg:text-[25px]">
+                <Tab>All Rooms</Tab>
+                <Tab>Available Rooms</Tab>
+              </div>
+            </TabList>
           </div>
 
           <TabPanel>
-          <div>
-      <h1>Room Filtering System</h1>
-      <div>
-        <label>Minimum Price:</label>
-        <input type="number" value={minPrice} onChange={handleMinPriceChange} />
-      </div>
-      <div>
-        <label>Maximum Price:</label>
-        <input type="number" value={maxPrice} onChange={handleMaxPriceChange} />
-      </div>
-    </div>
+
+            <div className="mt-5 flex flex-col items-center justify-center">
+              <h1 className="mb-3 text-xl underline font-bold">Room Filtering System</h1>
+              <div className="mb-2 mt-2">
+                <label>Minimum Price:</label> 
+                <input className="ml-5 border p-1"
+                  type="text"
+                  value={minPrice}
+                  placeholder="Enter min price"
+                  onChange={handleMinPriceChange}
+                />
+              </div>
+              <div className="mb-2 mt-2">
+                <label>Maximum Price:</label>
+                <input 
+                className="ml-5 border p-1"
+                  type="text"
+                  value={maxPrice}
+                  placeholder="Enter max price"
+                  onChange={handleMaxPriceChange}
+                />
+              </div>
+            </div>
+
             <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
               <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 gap-5">
                 {rooms?.map((room) => (
@@ -70,6 +80,28 @@ const Rooms = () => {
             </div>
           </TabPanel>
           <TabPanel>
+          <div className="mt-5 flex flex-col items-center justify-center">
+              <h1 className="mb-3 text-xl underline font-bold">Room Filtering System</h1>
+              <div className="mb-2 mt-2">
+                <label>Minimum Price:</label> 
+                <input className="ml-5 border p-1"
+                  type="text"
+                  value={minPrice}
+                  placeholder="Enter min price"
+                  onChange={handleMinPriceChange}
+                />
+              </div>
+              <div className="mb-2 mt-2">
+                <label>Maximum Price:</label>
+                <input 
+                className="ml-5 border p-1"
+                  type="text"
+                  value={maxPrice}
+                  placeholder="Enter max price"
+                  onChange={handleMaxPriceChange}
+                />
+              </div>
+            </div>
             <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
               <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 gap-5">
                 {rooms
